b11e9408bbba64d409f3e6dabaf6bf29110d895e
yuriagopian <yuri.agopian@hotmail.com>
2022-12-20 10:09:25 -0300

    chore: add  module imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersSchema } from './schema/users.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
