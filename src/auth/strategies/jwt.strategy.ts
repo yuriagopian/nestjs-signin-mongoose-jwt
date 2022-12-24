1d6dee135ba8e41bd91619ce9bad5f2dbb507063
yuriagopian <yuri.agopian@hotmail.com>
2022-12-23 21:44:06 -0300

    chore: jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: authService.returnJwtExtractor(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate() {}
+}
