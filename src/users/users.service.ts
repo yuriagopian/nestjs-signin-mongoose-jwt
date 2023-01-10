324d122e68961ae292c318788851d540dd0818e7
yuriagopian <yuri.agopian@hotmail.com>
2023-01-09 23:32:42 -0300

    chore: implementes users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { AuthService } from 'src/auth/auth.service';
+import { SignupDto } from './dto/singup.dto';
+import { User } from './models/users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel('User')
+    private readonly usersModel: Model<User>,
+    private readonly authService: AuthService,
+  ) {}
+
+  public async signup(signupDto: SignupDto): Promise<User> {
+    const user = new this.usersModel(signupDto);
+    return user.save();
+  }
+}
