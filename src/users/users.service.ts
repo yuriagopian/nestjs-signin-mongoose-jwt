316616597b7dc8ab53859c6b310de636005b19a0
yuriagopian <yuri.agopian@hotmail.com>
2023-01-09 23:42:29 -0300

    chore: implementes users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { Model } from 'mongoose';
 import { AuthService } from 'src/auth/auth.service';
+import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/singup.dto';
 import { User } from './models/users.model';
 
@@ -17,5 +18,21 @@ export class UsersService {
   public async signup(signupDto: SignupDto): Promise<User> {
     const user = new this.usersModel(signupDto);
     return user.save();
+  }
+
+  public async signin(
+    signinDto: SigninDto,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    const user = await this.findByEmail(signinDto.email);
+  }
+
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.usersModel.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 }
