bed3fd1b6b12630a07f87192559db9b420899ef1
yuriagopian <yuri.agopian@hotmail.com>
2023-01-09 23:26:39 -0300

    chore: implementes jwt payload model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { sign } from 'jsonwebtoken';
 import { Model } from 'mongoose';
 import { User } from 'src/users/models/users.model';
 import { Request } from 'express';
+import { JwtPayload } from './models/jwt-payload.model';
 
 @Injectable()
 export class AuthService {
@@ -28,9 +29,9 @@ export class AuthService {
     return sign(payload, secret, signOptions);
   }
 
-  public async validateUser(userId: string): Promise<User> {
+  public async validateUser(jwtPayload: JwtPayload): Promise<User> {
     const user = await this.usersModel.findOne({
-      _id: userId,
+      _id: jwtPayload.userId,
     });
 
     if (!user) {
